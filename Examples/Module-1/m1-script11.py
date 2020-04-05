c0e796768b97b50ca945a86d27c1d4fc38e16d03
ptoraska <ptoraska@redhat.com>
2020-04-05 22:19:01 +0530

    Update             # Python program to demonstrate break, continue and pass

---
+++ Examples/Module-1/m1-script11.py
@@ -0,0 +1,16 @@
+""" This is python script: Explain decision making or Conditional Statement!"""
+
+# Python program to demonstrate decision making
+
+num_1 = 10
+num_2 = 15
+
+# if to check even number
+if num_1 % 2 == 0:
+    print("Even Number")
+
+# if-else to check even or odd
+if num_2 % 2 == 0:
+    print("Even Number")
+else:
+    print("Odd Number")
