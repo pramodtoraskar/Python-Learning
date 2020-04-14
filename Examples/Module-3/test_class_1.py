31f28c635d242ce80adf40122f36672efffc1025
ptoraska <ptoraska@redhat.com>
2020-04-15 01:12:09 +0530

    Update Readme add new Module 4 chapter-Pandas & Matplotlib with examples

--- Examples/Module-3/test_class_1.py
+++ Examples/Module-3/test_class_1.py
@@ -3,13 +3,17 @@ class Parent():
     parent_attr = 100
 
     def __init__(self):
+        self.__test_var = 2020
         print("Calling parent constructor")
 
-    def parent_method(self):
+    def __parent_method(self):
+        print(self.test_var)
         print('Calling parent method')
 
     def set_attr(self, attr):
         Parent.parent_attr = attr
+        self.__test_var = 2030
 
     def get_attr(self):
         print("Parent attribute :", Parent.parent_attr)
+        print("Test Var: ", self.__test_var)
